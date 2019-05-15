ff9856d4c37cb286e1baed387729014be8a70a00
Hitesh Dua <hiteshdua1@gmail.com>
2019-05-15 23:37:22 +0530

    MInor diff pixels changes !

--- nodeDiff.js
+++ nodeDiff.js
@@ -7,10 +7,18 @@ var img1 = fs.createReadStream('ele.png').pipe(new PNG()).on('parsed', doneReadi
     filesRead = 0;
 
 function doneReading() {
-    if (++filesRead < 2) return;
+    
+    if (++filesRead < 2) {
+        return;
+    }
+
     var diff = new PNG({width: img1.width, height: img1.height});
 
-    console.log(pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {threshold: 0.1}));
-
+    const diffPixels = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {threshold: 0.1});
+    if (diffPixels>0) {
+        console.log(`Selector Image modified/changed, by ${diffPixels} Pixels Please validate !`);
+    } else {
+        console.log('Perfection Match !');
+    }
     diff.pack().pipe(fs.createWriteStream('diff.png'));
 }
